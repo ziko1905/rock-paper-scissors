e70e3cdf1b0a5ea47e942c635d1bb99c5965b378
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-03 15:17:16 +0200

    have fucntion for getting computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, Player!");
+
+function getComputerChoice() {
+    let random = Math.random();
+    let choice = Number(random * 3);
+    if (choice === 0) {
+        return "Rock";
+    } else if (choice === 1) {
+        return "Scissors";
+    } else {
+        return "Paper";
+    }
+}
