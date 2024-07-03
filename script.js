d95fca846fbdb01bcc9e5c4435e06592dd948634
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-03 16:08:49 +0200

    first version of game logic finished

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 console.log("Hello, Player!");
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice()
+
+    let winStr = `Congratulations! ${playerChoice} beats ${computerChoice}!`;
+    let loseStr = `Aww, ${computerChoice} beats ${playerChoice}!`
+
+    if (computerChoice === playerChoice) {
+        console.log(`Its a tie, you both selected ${computerChoice}`);
+    } else if (computerChoice === "Rock") {
+        if (playerChoice === "Paper") {
+            console.log(winStr);
+        } else {
+            console.log(loseStr);
+        }
+    } else if (computerChoice === "Paper") {
+        if (playerChoice === "Scissors") {
+            console.log(winStr);
+        } else {
+            console.log(loseStr);
+        }
+    } else {
+        if (playerChoice === "Rock") {
+            console.log(winStr);
+        } else {
+            console.log(loseStr);
+        }
+    }
+
+    console.log(`Total score is ${playerScore} for you and ${computerScore} for computer!`)
+    if (confirm("Want to play again!")) {
+        playRound()
+    }
+
+}
 
 function getComputerChoice() {
     let random = Math.random();
@@ -18,7 +56,7 @@ function getPlayerChoice () {
     while (!playerChoice) {
         playerChoice = prompt("Rock, Paper or Scissors? What is your choice?");    
     }
-    
+
     switch (playerChoice.toLocaleLowerCase()) {
         case "rock":
             return "Rock"
@@ -30,3 +68,5 @@ function getPlayerChoice () {
             return getPlayerChoice()
     }
 }
+
+playRound()
