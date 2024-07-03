0d5d8b4e9fe2b96afb1af936d0616fc3d57f0f3f
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-03 15:44:33 +0200

    have function for players choice

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ console.log("Hello, Player!");
 function getComputerChoice() {
     let random = Math.random();
     let choice = Number(random * 2).toFixed();
-    console.log(choice);
     if (choice == 0) {
         return "Rock";
     } else if (choice == 1) {
         return "Scissors";
     } else {
         return "Paper";
+    }
+}
+
+function getPlayerChoice () {
+    let playerChoice = null;
+
+    while (!playerChoice) {
+        playerChoice = prompt("Rock, Paper or Scissors? What is your choice?");    
+    }
+    
+    switch (playerChoice.toLocaleLowerCase()) {
+        case "rock":
+            return "Rock"
+        case "paper":
+            return "Paper"
+        case "scissors":
+            return "Scissors"
+        default:
+            return getPlayerChoice()
     }
 }
