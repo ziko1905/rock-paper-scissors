96c8d8f395ecf8e689ce3c269d445fec40f61565
Karlo Cehulic <karlocehlic@gmail.com>
2024-07-03 15:22:45 +0200

    fix getComputerChoice function so it works

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ console.log("Hello, Player!");
 
 function getComputerChoice() {
     let random = Math.random();
-    let choice = Number(random * 3);
-    if (choice === 0) {
+    let choice = Number(random * 2).toFixed();
+    console.log(choice);
+    if (choice == 0) {
         return "Rock";
-    } else if (choice === 1) {
+    } else if (choice == 1) {
         return "Scissors";
     } else {
         return "Paper";
